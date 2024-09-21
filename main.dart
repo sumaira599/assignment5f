9404b00575d285c5d7b0d5ba40b9d31c87430425
sumaira599 <samaraislam84@gmail.com>
2024-09-22 03:03:44 +0500

    done

---
+++ main.dart
@@ -0,0 +1,62 @@
+//  Revise the Topics
+// 1. If-Else
+// 2. List
+// 3. Map
+// 4. Loop
+// 5. Data Types
+ 
+ 
+//  import 'dart:io';
+
+// import 'dart:io';
+
+void main(){
+  // task #  if-else
+  // var percentage = stdin.readLineSync();
+  // if(percentage == "80%"){
+  //   print("you got A+ grade");
+  // }else if(percentage == "70"){
+  // print("you got A grade");
+  // }else if (percentage == "60"){
+  // print("you got B grade");
+  // }else if(percentage == "50"){
+  // print("you got C grade");
+  // }else if(percentage == "40"){
+  // print("you got D grade");
+  // }else if(percentage == "30"){
+  // print("you got E grade");
+  // }else{
+  //   print("failed");
+  // }
+   
+  //  task # 2 List
+
+  // List students = ["sara","ali","sana","dua","hasan","wafa"];
+  // students[4]="dawood";
+  // print(students);
+    
+  // task # 3 Map
+
+  // Map fruits = {
+  //   "apple": "RED",
+  // "mango" : "YELLOW",
+  // "orange" : "ORANGE",
+  // "grapes" : "GREEN"};
+  // var newFruits = fruits.map((key,value)=> MapEntry(key.toUpperCase(),value.toLowerCase()));
+  // print(newFruits);
+
+  // task # 4 loop
+
+  // List flowers = ["jasmine","rose","lily","lotus"];
+  // for(var i=0; i < flowers.length; i++){
+  //   var newFlowers = flowers[i];
+  //   print(newFlowers);
+  // };
+  
+//   var i = 0;
+// while(i < flowers.length){
+//   var newFlowers = flowers[i];
+//   i++;
+//   print(newFlowers);
+// }
+}
